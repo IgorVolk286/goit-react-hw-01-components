3e8efb1845554e5688535cdc0cb3ce0412e77af3
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-08-16 21:28:58 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import user from '../components/user.json';
+import { Profile } from './Profile.js/Profile';
+import { StatItem } from './Statistics.js/statitem';
+import data from '../components/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
